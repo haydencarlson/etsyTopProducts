ff4247f6d706ed485976de02c9194649366f71c9
Hayden Carlson <haydog2@hotmail.com>
2017-05-10 18:53:10 -0700

    Initial commit

---
+++ src/Listings.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Listing extends Component {
+
+  componentWillMount
+  render() {
+    return (
+      <div className="listingsContainer">
+
+      </div>
+    );
+  }
+}
+
+export default App;
