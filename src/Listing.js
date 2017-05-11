68f1b29e3bdd60272ea4c5d586c4b802da9a13d8
Hayden Carlson <haydog2@hotmail.com>
2017-05-10 20:10:49 -0700

    Added p tag to title

--- src/Listing.js
+++ src/Listing.js
@@ -5,8 +5,10 @@ class Listings extends Component {
   render() {
     return (
       <div>
-        <img alt="Listing" src={this.props.listing.Images[0].url_75x75}/>
-        <li>{this.props.listing.title}</li>
+        <li>
+          <img alt="Listing" src={this.props.listing.Images[0].url_75x75}/>
+          <p>{this.props.listing.title}</p>
+        </li>
         <span>{this.props.listing.price}</span>
         <hr/>
       </div>
