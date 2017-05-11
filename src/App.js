01445df81b71dcb1f2a84aa6fd0212d130f046e7
Hayden Carlson <haydog2@hotmail.com>
2017-05-10 19:50:04 -0700

    Fetch initial listings from etsy. Created renderListings func

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import Listings_component from './Listings.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Listings_component/>
       </div>
     );
   }
