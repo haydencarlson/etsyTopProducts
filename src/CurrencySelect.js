e68cb27a75c54154e551de913f8e665bd90fd795
Hayden Carlson <haydog2@hotmail.com>
2017-05-10 22:53:37 -0700

    Added currencyselect component. Fetches new rates from fixer io

--- src/CurrencySelect.js
+++ src/CurrencySelect.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+import './CurrencySelect.css';
 const currencies = ['GBP', 'CAD', 'USD', 'EUR'];
 class CurrencySelect extends Component {
 
   renderCurrency = (currencies) => {
-    return currencies.map((currency) => {
-      return (
-        <li> {currency} </li>
-      )
+    return currencies.map((currency, index) => {
+      if (this.props.currentCurrency !== currency) {
+        return (
+          <li key={index}
+              onClick={() => this.props.changeCurrency(currency)}
+              className="currencySelectLi"> {currency}
+          </li>
+        )
+      } else {
+        return (
+          <li key={index} className="currencySelectLi">
+              {currency}
+          </li>
+        )
+      }
     });
   }
   render() {
     return (
-      <div>
-        <span> Select a Currency </span>
-        <ul>
+      <div className="currencySelect">
+        <span> Select a Currency : </span>
+        <ul className="currencySelectUl">
           {this.renderCurrency(currencies)}
         </ul>
       </div>
